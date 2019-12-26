96f506c3f4c6ceeaadd4fbf318892d4392cc9fa5
Talhi <talhimohammed507@gmail.com>
2019-12-27 00:33:32 +0100

    gdgh

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,16 +1,7 @@
 package com.company;
-import java.sql.*;
 public class Main {
 
     public static void main(String[] args) {
-        try {
-            String host = "jdbc:mysql://localhost:3306/dotproject";
-            String uName = "root";
-            String uPass = "";
-            Connection con = DriverManager.getConnection(host, uName, uPass);
-        } catch (SQLException err) {
-            System.out.println(err.getMessage());
-
-        }
+        System.out.println();
     }
 }
